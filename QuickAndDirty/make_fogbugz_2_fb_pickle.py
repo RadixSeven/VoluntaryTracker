aa60d5bbd78845d8e443c38325f1617510275a9c
Eric David Moyer <eric_moyer@yahoo.com>
2013-02-21 04:02:22 -0500

    Made the last upload dates include timezone data

--- QuickAndDirty/make_fogbugz_2_fb_pickle.py
+++ QuickAndDirty/make_fogbugz_2_fb_pickle.py
@@ -2,14 +2,54 @@
 from __future__ import print_function
 import argparse, os.path, datetime, sys, pickle, errno
 
+# Local timezone class from Python 2 datetime documentation 
+from datetime import timedelta
+import time as _time
+
+STDOFFSET = timedelta(seconds = -_time.timezone)
+if _time.daylight:
+    DSTOFFSET = timedelta(seconds = -_time.altzone)
+else:
+    DSTOFFSET = STDOFFSET
+
+DSTDIFF = DSTOFFSET - STDOFFSET
+
+class LocalTimezone(datetime.tzinfo):
+
+    def utcoffset(self, dt):
+        if self._isdst(dt):
+            return DSTOFFSET
+        else:
+            return STDOFFSET
+
+    def dst(self, dt):
+        if self._isdst(dt):
+            return DSTDIFF
+        else:
+            return ZERO
+
+    def tzname(self, dt):
+        return _time.tzname[self._isdst(dt)]
+
+    def _isdst(self, dt):
+        tt = (dt.year, dt.month, dt.day,
+              dt.hour, dt.minute, dt.second,
+              dt.weekday(), 0, 0)
+        stamp = _time.mktime(tt)
+        tt = _time.localtime(stamp)
+        return tt.tm_isdst > 0
+
 # Utility function used by the command line arguments to parse a date
 def day_month_year_date(date_string):
     try:
         d = datetime.datetime.strptime(date_string, '%d %b %Y')
+        d = d.replace(tzinfo=LocalTimezone())
         return d
     except:
         msg = "The date must be in the format: 25 Dec 1955"
         raise argparse.ArgumentTypeError(msg)
+
+
 
 # Set up directory location constants
 #
