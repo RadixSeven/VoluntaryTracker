a1a7a7123a1f428f945f290ce5909b92e6fdcf76
Eric David Moyer <eric_moyer@yahoo.com>
2013-02-25 03:48:17 -0500

    Added the datetime from string convenience function and some tests of split_into_days based on it. Removed the rest of the erroneously written tests of split_into_days.

--- Fogzap/tests/test_fogzap.py
+++ Fogzap/tests/test_fogzap.py
@@ -16,6 +16,46 @@ from fogzap import CommandWithoutLogin
 from fogzap import list_commands
 from fogzap import run_fogzap
 
+
+import datetime
+def datetime_from_19_char_string(s):
+    """String in format: '20130221_0107030500'
+                          YYYYMMDD_HHmmSStimz
+                          0123456789012345678
+                                    111111111
+
+    The time zone is negative so 0500 would -5 hours and 0 minutes
+
+    """
+    year = int(s[0:4])
+    month = int(s[4:6])
+    day = int(s[6:8])
+    hour = int(s[9:11])
+    minute = int(s[11:13])
+    second = int(s[13:15])
+    tz_hour = -int(s[15:17])
+    tz_min  = -int(s[17:19])
+
+    class tz(datetime.tzinfo):
+        def utcoffset(self, dt):
+            return datetime.timedelta(hours = tz_hour, minutes = tz_min)
+
+        def dst(self, dt):
+            return timedelta(0)
+
+        def tzname(self, dt):
+            return None
+    
+    try:
+        return datetime.datetime(year, month, day, hour, minute, 
+                                 second, 0, tz())
+    except ValueError as e:
+        raise ValueError("Original message: '{}' params: {} {} {} {} {} {}"
+                         .format(str(e), year, month, day, hour, minute, second)
+                         )
+
+
+
 class TestReadConfigurationVariables(unittest.TestCase):
     def test_read_configuration_variables_returns_tuple(self):
         self.assertEqual(tuple, type(read_configuration_variables()))
@@ -351,146 +391,22 @@ class TestTimeIntervalForDayContaining(unittest.TestCase):
 
 class TestSplitIntoDays(unittest.TestCase):
     def test_split_into_days_returns_list_for_20130221_0000000500__20130221_0106500500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
+        first = datetime_from_19_char_string('20130221_0000000500')
+        last  = datetime_from_19_char_string('20130221_0106500500')
+        to_split = BoundedTimeInterval(first, last)
+        self.assertEqual([to_split], split_into_days(to_split))
 
     def test_split_into_days_returns_list_for_20130221_0107030500__20130221_0525000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
+        first = datetime_from_19_char_string('20130221_0107030500')
+        last  = datetime_from_19_char_string('20130221_0525000500')
+        to_split = BoundedTimeInterval(first, last)
+        self.assertEqual([to_split], split_into_days(to_split))
 
-    def test_split_into_days_returns_list_for_20130221_0525000500__20130221_0551060500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1304000500__20130221_1325000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1325000500__20130221_1331090500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1331090500__20130221_1345410500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1346100500__20130221_1407160500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1407160500__20130221_1432300500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1432300500__20130221_1532380500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1532380500__20130221_1630000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1630000500__20130221_1646090500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1646090500__20130221_1736020500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1800500500__20130221_1802110500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1802110500__20130221_1811080500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1811080500__20130221_1814570500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1814570500__20130221_1904110500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_1904270500__20130221_2103300500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_2103300500__20130221_2129260500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130221_2129260500__20130221_2152300500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1059000500__20130222_1303000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1303000500__20130222_1337000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1337000500__20130222_1347000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1347000500__20130222_1623000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1623000500__20130222_1630000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1630000500__20130222_1819000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1819000500__20130222_1821000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1821000500__20130222_1824000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_1824260500__20130222_1853030500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_2147000500__20130222_2151000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_2221540500__20130222_2244000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130222_2244000500__20130222_2301180500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_0023000500__20130223_0106030500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_0106130500__20130223_0342000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_0342000500__20130223_0416000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_0416000500__20130223_0420410500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_1243000500__20130223_1323510500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_1323510500__20130223_1325160500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_1325160500__20130223_1422040500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_1434180500__20130223_1952310500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130223_2033460500__20130223_2300060500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130224_1431160500__20130224_1519390500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130224_1556000500__20130224_2359000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130225_0000000500__20130225_0012000500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130225_0012460500__20130225_0014540500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130225_0014540500__20130225_0016250500(self):
-        self.assertEqual([], split_into_days(BoundedTimeInterval()))
-
-    def test_split_into_days_returns_list_for_20130225_0016250500__Ongoing(self):
-        self.assertEqual([], split_into_days(OngoingTimeInterval()))
 
 class TestSummarizeIntervalsByDay(unittest.TestCase):
     def test_summarize_intervals_by_day_returns_dict_for_list(self):
-        self.assertEqual({}, summarize_intervals_by_day([Interval(964, 2, 805, BoundedTimeInterval(), False, 'Implement pickler'), Interval(965, 2, 806, BoundedTimeInterval(), False, 'Implement fogbugz download'), Interval(966, 2, 808, BoundedTimeInterval(), False, 'Push voluntary tracker to github'), Interval(968, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(967, 2, 765, BoundedTimeInterval(), False, 'Planning'), Interval(969, 2, 769, BoundedTimeInterval(), False, 'Status Meeting (14 Feb 2013)'), Interval(970, 2, 769, BoundedTimeInterval(), False, 'Status Meeting (14 Feb 2013)'), Interval(971, 2, 767, BoundedTimeInterval(), False, 'Computer maintenance'), Interval(972, 2, 765, BoundedTimeInterval(), False, 'Planning'), Interval(973, 2, 810, BoundedTimeInterval(), False, 'Move fogbugz direct stuff into a module (logon, queries, etc.)'), Interval(974, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(975, 2, 810, BoundedTimeInterval(), False, 'Move fogbugz direct stuff into a module (logon, queries, etc.)'), Interval(976, 2, 810, BoundedTimeInterval(), False, 'Move fogbugz direct stuff into a module (logon, queries, etc.)'), Interval(977, 2, 815, BoundedTimeInterval(), False, 'Reimplement fogbugz_2_fb using the library'), Interval(978, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(979, 2, 815, BoundedTimeInterval(), False, 'Reimplement fogbugz_2_fb using the library'), Interval(980, 2, 811, BoundedTimeInterval(), False, 'Create FogbugzCase class'), Interval(981, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(982, 2, 811, BoundedTimeInterval(), False, 'Create FogbugzCase class'), Interval(986, 2, 819, BoundedTimeInterval(), False, 'Sriram N job talk'), Interval(987, 2, 822, BoundedTimeInterval(), False, 'Talk with co-workers'), Interval(988, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(989, 2, 822, BoundedTimeInterval(), False, 'Talk with co-workers'), Interval(990, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(991, 2, 818, BoundedTimeInterval(), False, 'Meeting with Sriram N.'), Interval(992, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(993, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(983, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(984, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(985, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(994, 2, 812, BoundedTimeInterval(), False, 'Rename to fogzap and repurpose to being a fogbugz command line uitl that has as one of its functions uploading to facebook'), Interval(995, 2, 823, BoundedTimeInterval(), False, 'Move config file reading to its own file'), Interval(996, 2, 813, BoundedTimeInterval(), False, 'Implement a subcommand interface with commands CaseStats (unimplemented) and UnsharedIntervals'), Interval(997, 2, 824, BoundedTimeInterval(), False, 'Implement list of time worked for each un-uploaded day'), Interval(998, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(999, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(1000, 2, 816, BoundedTimeInterval(), False, 'Add foglib.Session.cases_named method'), Interval(1001, 2, 807, BoundedTimeInterval(), False, 'Implement facebook upload'), Interval(1002, 2, 807, BoundedTimeInterval(), False, 'Implement facebook upload'), Interval(1003, 2, 807, BoundedTimeInterval(), False, 'Implement facebook upload'), Interval(1004, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(1005, 2, 826, BoundedTimeInterval(), False, 'Refactor foglib.Interval'), Interval(1006, 2, 826, BoundedTimeInterval(), False, 'Refactor foglib.Interval'), Interval(1007, 2, 825, BoundedTimeInterval(), False, 'Add test cases for fogzap and libraries'), Interval(1008, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(1009, 2, 825, OngoingTimeInterval(), False, 'Add test cases for fogzap and libraries')]))
+        pass
+#        self.assertEqual({}, summarize_intervals_by_day([Interval(964, 2, 805, BoundedTimeInterval(), False, 'Implement pickler'), Interval(965, 2, 806, BoundedTimeInterval(), False, 'Implement fogbugz download'), Interval(966, 2, 808, BoundedTimeInterval(), False, 'Push voluntary tracker to github'), Interval(968, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(967, 2, 765, BoundedTimeInterval(), False, 'Planning'), Interval(969, 2, 769, BoundedTimeInterval(), False, 'Status Meeting (14 Feb 2013)'), Interval(970, 2, 769, BoundedTimeInterval(), False, 'Status Meeting (14 Feb 2013)'), Interval(971, 2, 767, BoundedTimeInterval(), False, 'Computer maintenance'), Interval(972, 2, 765, BoundedTimeInterval(), False, 'Planning'), Interval(973, 2, 810, BoundedTimeInterval(), False, 'Move fogbugz direct stuff into a module (logon, queries, etc.)'), Interval(974, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(975, 2, 810, BoundedTimeInterval(), False, 'Move fogbugz direct stuff into a module (logon, queries, etc.)'), Interval(976, 2, 810, BoundedTimeInterval(), False, 'Move fogbugz direct stuff into a module (logon, queries, etc.)'), Interval(977, 2, 815, BoundedTimeInterval(), False, 'Reimplement fogbugz_2_fb using the library'), Interval(978, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(979, 2, 815, BoundedTimeInterval(), False, 'Reimplement fogbugz_2_fb using the library'), Interval(980, 2, 811, BoundedTimeInterval(), False, 'Create FogbugzCase class'), Interval(981, 2, 766, BoundedTimeInterval(), False, 'Email and social networking'), Interval(982, 2, 811, BoundedTimeInterval(), False, 'Create FogbugzCase class'), Interval(986, 2, 819, BoundedTimeInterval(), False, 'Sriram N job talk'), Interval(987, 2, 822, BoundedTimeInterval(), False, 'Talk with co-workers'), Interval(988, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(989, 2, 822, BoundedTimeInterval(), False, 'Talk with co-workers'), Interval(990, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(991, 2, 818, BoundedTimeInterval(), False, 'Meeting with Sriram N.'), Interval(992, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(993, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(983, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(984, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(985, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(994, 2, 812, BoundedTimeInterval(), False, 'Rename to fogzap and repurpose to being a fogbugz command line uitl that has as one of its functions uploading to facebook'), Interval(995, 2, 823, BoundedTimeInterval(), False, 'Move config file reading to its own file'), Interval(996, 2, 813, BoundedTimeInterval(), False, 'Implement a subcommand interface with commands CaseStats (unimplemented) and UnsharedIntervals'), Interval(997, 2, 824, BoundedTimeInterval(), False, 'Implement list of time worked for each un-uploaded day'), Interval(998, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(999, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(1000, 2, 816, BoundedTimeInterval(), False, 'Add foglib.Session.cases_named method'), Interval(1001, 2, 807, BoundedTimeInterval(), False, 'Implement facebook upload'), Interval(1002, 2, 807, BoundedTimeInterval(), False, 'Implement facebook upload'), Interval(1003, 2, 807, BoundedTimeInterval(), False, 'Implement facebook upload'), Interval(1004, 2, 820, BoundedTimeInterval(), False, 'Email and social networking'), Interval(1005, 2, 826, BoundedTimeInterval(), False, 'Refactor foglib.Interval'), Interval(1006, 2, 826, BoundedTimeInterval(), False, 'Refactor foglib.Interval'), Interval(1007, 2, 825, BoundedTimeInterval(), False, 'Add test cases for fogzap and libraries'), Interval(1008, 2, 821, BoundedTimeInterval(), False, 'Planning'), Interval(1009, 2, 825, OngoingTimeInterval(), False, 'Add test cases for fogzap and libraries')]))
 
 class TestCommand(unittest.TestCase):
     def test_creation_with_function_equal_list_unshared_intervals_function_and_name_equal_list_unshared_intervals_and_short_desc_equal_Lists_the_time_intervals_that_have_not_yet_been_shared_on_social_media(self):
@@ -546,14 +462,10 @@ class TestListCommands(unittest.TestCase):
         pass
 
 class TestRunFogzap(unittest.TestCase):
-    def test_run_fogzap_returns_None_for_list(self):
-        self.assertEqual(None, run_fogzap(['./fogzap.py', 'list_unshared_intervals']))
-
     def test_run_fogzap_returns_None_for_list_case_2(self):
         self.assertEqual(None, run_fogzap(['./fogzap.py', 'list_commands']))
 
-    def test_run_fogzap_returns_None_for_list_case_3(self):
-        self.assertEqual(None, run_fogzap(['./fogzap.py', 'list_unshared_work_times']))
+
 
 if __name__ == '__main__':
     unittest.main()
