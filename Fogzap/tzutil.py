2de3ec47c2f5bdf990adabd8b2e007b69916c02d
Eric David Moyer <eric_moyer@yahoo.com>
2013-02-24 21:50:25 -0500

    I've added the support code (now() and in_future(datetime)) to LocalTimezone. Nothing is tested and fogzap.py still needs rewriting. It will break on the new class.

--- Fogzap/tzutil.py
+++ Fogzap/tzutil.py
@@ -17,7 +17,11 @@ class LocalTimezone(datetime.tzinfo):
 
     Probably has bugs where it doesn't work if the program is running
     while a daylight savings time or other time-zone irregularity is
-    crossed."""
+    crossed.
+
+    I've also added some extra methods not in the original: now and in_future
+
+    """
 
     def utcoffset(self, dt):
         if self._isdst(dt):
@@ -41,6 +45,28 @@ class LocalTimezone(datetime.tzinfo):
         stamp = _time.mktime(tt)
         tt = _time.localtime(stamp)
         return tt.tm_isdst > 0
+
+    def now(self):
+        """Return a timezone aware datetime.datetime that represents the current microsecond"""
+        return datetime.datetime.now(LocalTimezone())
+
+    def in_future(self, a_datetime):
+        """Return true if a_datetime comes strictly after the current microsecond.
+        
+        a_datetime must be a timezone aware datetime.datetime object
+
+        Note: there is a bit of a race condition in this code because
+        I have to calculate the 'now' object before I can compare it
+        to a_datetime. Thus if a_datetime lies in the interval between
+        the time the function is called and the time the instant of
+        the 'now' object is fixed this will return (incorrectly) that
+        a_datetime is not in the future. Another source of error is
+        system clock resolution and accuracy. For most applications,
+        this is completely inconsequential, but consider yourself
+        warned.
+
+        """
+        return a_datetime > self.now()
 
 class UTC(datetime.tzinfo):
     """UTC class from the tzinfo documentation quoted in: http://stackoverflow.com/questions/2331592/datetime-datetime-utcnow-why-no-tzinfo"""
